bcf1764d83395faa38c55e0c2c70796d2e0c392e
Shashi Sugrim <ssugrim1430@bths.edu>
2023-09-15 17:25:19 -0400

    basic functions to utilize crud tweet fns

--- src/main/java/com/devs/TwitterBackend/Controller/TweetController.java
+++ src/main/java/com/devs/TwitterBackend/Controller/TweetController.java
@@ -5,10 +5,7 @@ import com.devs.TwitterBackend.model.Tweet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +25,27 @@ public class TweetController {
     public ResponseEntity<List<Tweet>> findAll(){
         System.out.println("Get tweets is returning " + repository.getTweets());
         return new ResponseEntity<>(repository.getTweets(), HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping("")
+    public void createTweet(@RequestBody Tweet tweet)
+    {
+        System.out.println("This is tweet received " + tweet);
+        repository.insertTweet(tweet);
+    }
+
+    // to delete, send a request to postman, raw data, only an integer
+    @DeleteMapping("")
+    public void deleteTweet(@RequestBody Integer id)
+    {
+        System.out.println("We are receiving request to delete with id " + id);
+        repository.deleteTweet(id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("")
+    public void updateTweet(@RequestBody Tweet tweet)
+    {
+        repository.updateTweet(tweet);
     }
 }
